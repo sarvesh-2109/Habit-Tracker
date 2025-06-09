8f8041a2534d1ad647c9e0584a4659972c2aa9ee
sarvesh-2109 <sarveshmhatre213@gmail.com>
2025-06-09 21:41:11 +0530

    Can see the streak infor on the streak page. 3:39:30

--- app/(tabs)/streaks.tsx
+++ app/(tabs)/streaks.tsx
@@ -1,9 +1,141 @@
 import { View, Text } from "react-native";
+import { useAuth } from "@/lib/auth-context";
+import { useState, useEffect } from "react";
+import { Habit, HabitCompletion } from "@/types/database.type";
+import {
+  COMPLETIONS_COLLECTION_ID,
+  DATABASE_ID,
+  databases,
+  HABITS_COLLECTION_ID,
+} from "@/lib/appwrite";
+import { Query } from "react-native-appwrite";
+import { Card } from "react-native-paper";
 
 export default function StreaksScreen() {
+  const [habits, setHabits] = useState<Habit[]>([]); // Initialize with empty array
+  const [completedHabits, setCompletedHabits] = useState<HabitCompletion[]>([]); // Initialize with empty array
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (user?.$id) {
+      fetchHabits();
+      fetchCompletions();
+    }
+  }, [user]);
+
+  const fetchHabits = async () => {
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        HABITS_COLLECTION_ID,
+        [Query.equal("user_id", user!.$id)]
+      );
+      setHabits(response.documents as Habit[]);
+    } catch (error) {
+      console.error("Error fetching habits:", error);
+    }
+  };
+
+  const fetchCompletions = async () => {
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        COMPLETIONS_COLLECTION_ID,
+        [Query.equal("user_id", user!.$id)]
+      );
+      const completions = response.documents as HabitCompletion[];
+      setCompletedHabits(completions);
+    } catch (error) {
+      console.error("Error fetching completions:", error);
+    }
+  };
+
+  interface StreakData {
+    streak: number;
+    bestStreak: number;
+    total: number;
+  }
+
+  const getStreakData = (habitId: string): StreakData => {
+    const habitCompletions = completedHabits
+      ?.filter((c) => c.habit_id === habitId)
+      .sort(
+        (a, b) =>
+          new Date(a.completed_at).getTime() -
+          new Date(b.completed_at).getTime()
+      );
+
+    if (habitCompletions?.length === 0) {
+      return { streak: 0, bestStreak: 0, total: 0 };
+    }
+
+    // build streak data
+    let streak = 0;
+    let bestStreak = 0;
+    let total = habitCompletions.length;
+
+    let lastDate: Date | null = null;
+    let currentStreak = 0;
+
+    habitCompletions?.forEach((c) => {
+      const date = new Date(c.completed_at);
+      if (lastDate) {
+        const diff =
+          (date.getTime() - lastDate.getTime()) / (1000 * 60 * 60 * 24);
+        if (diff <= 1.5) {
+          currentStreak += 1;
+        } else {
+          currentStreak = 1;
+        }
+      } else {
+        if (currentStreak > bestStreak) bestStreak = currentStreak;
+        streak = currentStreak;
+        lastDate = date;
+      }
+    });
+
+    return { streak, bestStreak, total };
+  };
+
+  const habitStreaks = habits.map((habit) => {
+    const { streak, bestStreak, total } = getStreakData(habit.$id);
+    return { habit, bestStreak, streak, total };
+  });
+
+  const rankedHabits = habitStreaks.sort((a, b) => a.bestStreak - b.bestStreak);
+
   return (
     <View>
-      <Text> This is the streaks page</Text>
+      <Text> Habit Streaks </Text>
+
+      {habits.length === 0 ? (
+        <View>
+          <Text>No habits yet. Add your first habit.</Text>
+        </View>
+      ) : (
+        rankedHabits.map(({ habit, streak, bestStreak, total }, key) => (
+          <Card key={key}>
+            <Card.Content>
+              <Text>{habit.title}</Text>
+              <Text>{habit.description}</Text>
+              <View>
+                <View>
+                  <Text>🔥 {streak}</Text>
+                  <Text>Current</Text>
+                </View>
+                <View>
+                  <Text>🏆 {bestStreak}</Text>
+                  <Text>Best</Text>
+                </View>
+                <View>
+                  <Text>✅ {total}</Text>
+                  <Text>Total</Text>
+                </View>
+              </View>
+            </Card.Content>
+          </Card>
+        ))
+      )}
     </View>
   );
 }
